99b95c520620915dc382592c105fece5294dd15f
Jon Batista <batista.jonathas@gmail.com>
2021-03-30 17:43:09 -0300

    cors

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -26,7 +26,7 @@ class CourseController extends Controller
      */
     public function index()
     {
-        return response()->json([$this->course])->paginate(10);
+        return $this->course->paginate(10);
     }
     
     /**
